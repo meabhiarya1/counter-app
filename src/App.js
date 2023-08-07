da031ea62f44d1a55abfccadca5e8f3845f4a225
Abhishek kumar <abhi.arya97161@gmail.com>
2023-08-07 11:18:45 +0530

    Commit again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Asset from './Components/Asset';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+
+  const [counter,setCounter] = useState(0)
+
+  const handleClick1 = () =>{
+    setCounter(counter+1)
+  }
+
+  const handleClick2 = () =>{
+    if(counter != 0)
+    {
+      setCounter(counter-1)
+    }
+  }
+
+  const handleClick3 = () =>{
+    setCounter(counter === 0);
+    window.location.reload()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className='total_amount_card' style={{ backgroundImage: `linear-gradient(to right,  rgba(253, 230, 90, 100%), rgba(204, 254, 87, 100%))` }}>
+          <div className='right' >
+            <Asset onClick = {handleClick3}/>
+          </div>
+
+          <div className='card_text'>
+            <h3 className='total_amount_heading'>{counter}</h3>
+          </div>
+        </div>
+
+        <form>
+          <div className='button_collection'>
+            <Stack spacing={2} direction="row">
+              <Button onClick={handleClick1} variant="contained" className='btn_one'>+</Button>
+              <Button onClick={handleClick2} variant="contained" className='btn_two'>-</Button>
+            </Stack>
+          </div>
+        </form>
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/Asset.js
@@ -0,0 +1,8 @@
+const Asset = (props) =>{
+    return(
+        <svg onClick={props.onClick} xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="17" height="17"><path d="M21.962,12.875A10.03,10.03,0,1,1,19.122,5H16a1,1,0,0,0-1,1h0a1,1,0,0,0,1,1h4.143A1.858,1.858,0,0,0,22,5.143V1a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1V3.078A11.985,11.985,0,1,0,23.95,13.1a1.007,1.007,0,0,0-1-1.1h0A.982.982,0,0,0,21.962,12.875Z" /></svg>
+    )
+    
+}
+
+export default Asset;
